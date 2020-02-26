e300f7bada88bb07c9036d4be24ad254018c418b
JanakiTHBS <janakinaidu.pattipati@gmail.com>
2020-02-26 12:36:48 +0000

    observables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'user/:id', component: UserComponent}
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'observables';
+export class AppComponent implements OnInit,OnDestroy {
+  subscription:Subscription;
+  userActivated=false;
+  constructor(private userService:UserService){
+
+  }
+  ngOnInit(){
+    this.subscription=this.userService.activatedEmitter.subscribe((didActivated)=>{
+this.userActivated=didActivated;
+    });
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
